5932373a44284973cfe6be42716bc9f1d38f2835
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-02-16 18:06:34 +0530

    Added image to the card and styled it

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Card1 from "../images/card 1.jpeg"
+import "../styles/Card.css"
 
 function Card() {
   return (
-    <div>Card</div>
+    <div className='Card'>
+        <div className='card-container'>
+            <div className='card-img-container'>
+            <img className='card1' src={Card1} alt="card1"/>
+            </div>
+            <div className='card-info-container'>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
