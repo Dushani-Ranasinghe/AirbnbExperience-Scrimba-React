b41050c2ad14602f4e12a29f3da1851a0cc458b4
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-02-16 17:36:48 +0530

    Added Hero Image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import HeroImg from '../images/hero-img.png'
+import "../styles/Hero.css"
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='Hero'>
+      <div className='hero-img-container'>
+      <img className="Hero-img" src={HeroImg} alt="HeroImage"/>
+      <div className='hero-content'>
+        
+      </div>
+      </div>
+    </div>
   )
 }
 
