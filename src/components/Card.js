2553a30120ecdb47c24528365d34cc4dde7caf6c
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-02-16 18:57:25 +0530

    Added card info and styled it

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Card1 from "../images/card 1.jpeg"
+import Star from "../images/star.png"
 import "../styles/Card.css"
 
 function Card() {
   return (
     <div className='Card'>
         <div className='card-container'>
-            <div className='card-img-container'>
             <img className='card1' src={Card1} alt="card1"/>
+            <div className='card-stat'>
+                <img src={Star} alt="voting_icon" className='voting_icon'/>
+                <span>5.0</span>
+                <span>(6) • </span>
+                <span>USA</span>
             </div>
-            <div className='card-info-container'>
-
+            <div className='card-info'>
+            <p>Life lessons with Katie Zaferes</p>
+            <p className='cost'><b>From $136</b> / person</p>
             </div>
         </div>
     </div>
