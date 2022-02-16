4fafed31c82b2b637aa4bf1f2950797d64eccabe
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-02-16 17:49:00 +0530

    Styled Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,10 +5,14 @@ import "../styles/Hero.css"
 function Hero() {
   return (
     <div className='Hero'>
+      <div className='hero-container'>
       <div className='hero-img-container'>
       <img className="Hero-img" src={HeroImg} alt="HeroImage"/>
+      </div>
       <div className='hero-content'>
-        
+        <h1>Online Experinece</h1>
+        <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      
       </div>
       </div>
     </div>
