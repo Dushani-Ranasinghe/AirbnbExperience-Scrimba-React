3acef3f7a0b2813fb412be9117fb17deb969753a
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-02-18 03:54:00 +0530

    Added images to cards

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,12 +1,56 @@
 import React from 'react'
 import CardItem from './CardItem'
-import Image from "../images/katie.png"
+import Katie from "../images/katie.png"
+import Bride from "../images/bride.jpg"
+import Bike from "../images/bike.jpg"
+import Yoga from "../images/yoga.jpg"
+import Paint from "../images/paint.jpg"
 
 function CardList() {
   return (
     <div>
         <CardItem
-        image={Image}
+        image={Katie}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price="136"
+        />
+          <CardItem
+        image={Katie}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price="136"
+        />
+            <CardItem
+        image={Bride}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price="136"
+        />
+            <CardItem
+        image={Bike}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price="136"
+        />
+            <CardItem
+        image={Yoga}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price="136"
+        />
+        <CardItem
+        image={Paint}
         rating="5.0"
         reviewCount={6}
         country="USA"
